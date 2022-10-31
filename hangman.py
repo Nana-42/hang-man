836ba2cc1d4bb2d49c54d0736956ec41422e3185
Haider.S <99351349+Nana-42@users.noreply.github.com>
2022-10-31 00:09:54 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,21 @@
+print("Welcome to hangman")
+word = input("Enter a word to guess: ")
+
+counter = False
+for n in range(len(word)):
+    new = '_'
+    print(new, end=' ')
+
+guess = input('\nEnter a character: ')
+
+while(counter == False):
+    for n in word:
+        if(guess == n):
+            print("You have guessed", n)
+            gues
+            word = word.replace(n, '')
+            for i in range(len(word)):
+                print(new, end=' ')
+        else:
+            print("False! Enter aother letter")
+            counter = True
